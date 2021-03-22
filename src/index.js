21fbbcbbfbe0380a21797ea8f802ae6213f0b21b
mshumsky <mshumsky@outlook.com>
2021-03-22 23:21:16 +0300

    Added support of params in script src as query

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import "./scss/index.scss";
 import Widget from "./js/Widget";
 
-new Widget({mode: "rect"});
+/* Load params from script src attribute query */
+
+const src = document.currentScript.getAttribute("src");
+const query = src.split("?");
+query.shift();
+
+const params = {};
+query.forEach((param) => {
+	const split = param.split("=");
+	params[split[0]] = split[1];
+});
+
+/* Run */
+
+new Widget(params);
